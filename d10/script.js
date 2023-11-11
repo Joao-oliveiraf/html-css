f6dc69428f1fc067ae2ab5b92a0b545787fd89b9
João Pedro Freitas de Oliveira <jpfreitas15@hotmail.com>
2023-11-11 17:21:34 -0300

    Changes to d10: fixing Section, UL responsive IMGS

--- d10/script.js
+++ d10/script.js
@@ -1,10 +1,10 @@
 function resize(){
     let w = window.innerWidth;
     if (w < 600){
-        document.getElementById('irina').scr = 'imagens/irina-blok-pq.jpg'
-        document.getElementById('dandroids').scr = 'imagens/dan-droids-pq.png'
+        document.getElementById('irina').src = 'imagens/irina-blok-pq.jpg'
+        document.getElementById('dandroids').src = 'imagens/dan-droids-pq.png'
     }else{
-        document.getElementById('irina').scr = 'imagens/irina-blok.jpg'
-        document.getElementById('dandroids').scr = 'imagens/dan-droids.png'
+        document.getElementById('irina').src = 'imagens/irina-blok.jpg'
+        document.getElementById('dandroids').src = 'imagens/dan-droids.png'
     }
 }
